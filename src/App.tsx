8ed2e62b937a62873138bc6b88d1cce349c402e1
Vy <nguyenhuutrongvy@gmail.com>
2025-06-13 09:44:18 +0700

    feat: install and configure @tanstack/react-query and devtools
    - Add @tanstack/react-query and @tanstack/react-query-devtools dependencies
    - Set up QueryClient and QueryClientProvider in App.tsx
    - Integrate ReactQueryDevtools for development

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Account from "./pages/Account";
@@ -11,9 +13,17 @@ import NewUsers from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +40,8 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
